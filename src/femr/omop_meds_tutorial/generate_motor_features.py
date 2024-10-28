02b2e1ae872efe4a9679e6a6a43a7f18c405802c
Chao Pang <ChaoPang229@gmail.com>
2024-10-28 12:30:58 -0400

    added generate_motor_features.py

---
+++ src/femr/omop_meds_tutorial/generate_motor_features.py
@@ -0,0 +1,40 @@
+import femr.transforms
+import meds_reader
+import femr.models.transformer
+import pandas as pd
+import os
+import pickle
+import meds
+import pathlib
+import torch
+from .generate_labels import create_omop_meds_tutorial_arg_parser, LABEL_NAMES
+
+
+def main():
+    args = create_omop_meds_tutorial_arg_parser().parse_args()
+    with meds_reader.SubjectDatabase(args.meds_reader, num_threads=6) as database:
+        pretraining_data = pathlib.Path(args.pretraining_data)
+        ontology_path = pretraining_data / 'ontology.pkl'
+
+        with open(ontology_path, 'rb') as f:
+            ontology = pickle.load(f)
+
+        for label_name in LABEL_NAMES:
+            labels = pd.read_parquet(
+                pretraining_data / "labels" / (label_name + '.parquet')
+            )
+            features = femr.models.transformer.compute_features(
+                db=database,
+                model_path=str(pretraining_data / "motor_model"),
+                labels=labels.to_dict(orient="records"),
+                ontology=ontology,
+                device=torch.device('cuda'),
+                tokens_per_batch=32 * 1024,
+                num_proc=6
+            )
+            with open(pretraining_data / "features" / (label_name + '_motor.pkl'), 'wb') as f:
+                pickle.dump(features, f)
+
+
+if __name__ == "__main__":
+    main()
