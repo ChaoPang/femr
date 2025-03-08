8ddc560efa1d4ec5b68274e98a21d2a013c6fe8b
Chao Pang <ChaoPang229@gmail.com>
2025-03-08 00:08:57 -0500

    extended generate_motor_features to allow custom cohorts

--- src/femr/omop_meds_tutorial/generate_motor_features.py
+++ src/femr/omop_meds_tutorial/generate_motor_features.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import femr.transforms
 import meds_reader
 import femr.models.transformer
@@ -8,23 +10,35 @@ import pathlib
 import torch
 from .generate_labels import create_omop_meds_tutorial_arg_parser, LABEL_NAMES
 
+
 def create_arg_parser():
     args = create_omop_meds_tutorial_arg_parser()
     args.add_argument(
-        "num_proc",
+        "--num_proc",
+        dest="num_proc",
         type=int,
-        required=False,
         default=6,
-        help="Number of processes to use"
+        help="Number of processes to use",
     )
     args.add_argument(
-        "tokens_per_batch",
+        "--tokens_per_batch",
+        dest="tokens_per_batch",
         type=int,
-        required=False,
         default=32 * 1024,
-        help="The number of tokens per batch to use"
+        help="The number of tokens per batch to use",
+    )
+    args.add_argument(
+        "--cohort_dir",
+        dest="cohort_dir",
+        default=None,
     )
     return args
+
+def read_recursive_parquet(root_dir):
+    all_files = glob.glob(os.path.join(root_dir, '**', '*.parquet'), recursive=True)
+    df = pd.concat((pd.read_parquet(f) for f in all_files), ignore_index=True)
+    return df
+
 
 def main():
     args = create_arg_parser().parse_args()
@@ -35,7 +49,26 @@ def main():
         with open(ontology_path, 'rb') as f:
             ontology = pickle.load(f)
 
-        for label_name in LABEL_NAMES:
+        labels = LABEL_NAMES
+        if args.cohort_dir is not None:
+            if os.path.isdir(args.cohort_dir):
+                label_name = os.path.basename(os.path.normpath(args.cohort_dir))
+                cohort = read_recursive_parquet(args.cohort_dir)
+            else:
+                label_name = os.path.basename(os.path.splitext(args.cohort_dir)[0])
+                file_extension = os.path.splitext(args.cohort_dir)[1]
+                if file_extension.lower() == ".parquet":
+                    cohort = pd.read_parquet(args.cohort_dir)
+                elif file_extension.lower() == ".csv":
+                    cohort = pd.read_csv(args.cohort_dir)
+                else:
+                    raise RuntimeError(f"Unknown file extension: {file_extension}")
+            cohort.to_parquet(
+                pretraining_data / "labels" / (label_name + '.parquet')
+            )
+            labels = [label_name]
+
+        for label_name in labels:
             labels = pd.read_parquet(
                 pretraining_data / "labels" / (label_name + '.parquet')
             )
