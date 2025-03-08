017ca56e868fb4416073ef2566f336bfa1696011
Chao Pang <ChaoPang229@gmail.com>
2025-03-07 19:53:33 -0500

    added aditional arguments for pretrain_motor.py

--- src/femr/omop_meds_tutorial/pretrain_motor.py
+++ src/femr/omop_meds_tutorial/pretrain_motor.py
@@ -9,8 +9,7 @@ import femr.models.tokenizer
 import femr.models.processor
 from .generate_labels import create_omop_meds_tutorial_arg_parser
 
-
-def main():
+def create_arg_parser():
     arg_parser = create_omop_meds_tutorial_arg_parser()
     arg_parser.add_argument(
         "--learning_rate",
@@ -18,7 +17,34 @@ def main():
         type=float,
         default=1e-4
     )
-    args = arg_parser.parse_args()
+    arg_parser.add_argument(
+        "--n_layers",
+        dest="n_layers",
+        type=int,
+        default=12
+    )
+    arg_parser.add_argument(
+        "--n_epochs",
+        dest="n_epochs",
+        type=int,
+        default=100
+    )
+    arg_parser.add_argument(
+        "--per_device_train_batch_size",
+        dest="per_device_train_batch_size",
+        type=int,
+        default=1
+    )
+    arg_parser.add_argument(
+        "--per_device_eval_batch_size",
+        dest="per_device_eval_batch_size",
+        type=int,
+        default=1
+    )
+    return arg_parser
+
+def main():
+    args = create_arg_parser().parse_args()
     pretraining_data = pathlib.Path(args.pretraining_data)
 
     ontology_path = pretraining_data / 'ontology.pkl'
@@ -50,7 +76,7 @@ def main():
     transformer_config = femr.models.config.FEMRTransformerConfig(
         vocab_size=tokenizer.vocab_size,
         is_hierarchical=isinstance(tokenizer, femr.models.tokenizer.HierarchicalTokenizer),
-        n_layers=6,
+        n_layers=args.n_layers,
         use_normed_ages=True,
         use_bias=False,
         hidden_act='swiglu',
@@ -65,8 +91,8 @@ def main():
     learning_rate = args.learning_rate
 
     trainer_config = transformers.TrainingArguments(
-        per_device_train_batch_size=1,
-        per_device_eval_batch_size=1,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        per_device_eval_batch_size=args.per_device_eval_batch_size,
 
         learning_rate=learning_rate,
         output_dir='tmp_trainer_' + sys.argv[1],
@@ -78,7 +104,7 @@ def main():
 
         report_to=["tensorboard"],
 
-        num_train_epochs=100,
+        num_train_epochs=args.n_epochs,
 
         warmup_steps=500,
 
